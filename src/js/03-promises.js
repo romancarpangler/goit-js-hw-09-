543ddc51d15851dda5a87ac431bcfd8a463b4c83
romancarpangler <korzunroman56@gmail.com>
2022-12-25 17:40:33 +0200

    gg

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,7 +15,7 @@ function submitCreatePromises(e) {
   const delayStepMsVal = delayStepMs.valueAsNumber;
   const amountVal = amount.valueAsNumber;
 
-  for (let i = 1; i <= amountVal; i++) {
+  for (let i = 1; i <= amountVal; i += 1) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${delay}ms`);
